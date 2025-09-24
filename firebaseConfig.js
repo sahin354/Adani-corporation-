16d5ee7e27c78c860e4234f1e0534a0957cf4d6c
sahin354 <aivlog59952@gmail.com>
2025-09-25 00:42:43 +0530

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,12 +1,12 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBqnJpGCtplUIwspovyntn9bbaTY2ygLNE",
-  authDomain: "adani-investment.firebaseapp.com",
-  projectId: "adani-investment",
-  storageBucket: "adani-investment.firebasestorage.app",
-  messagingSenderId: "549652082720",
-  appId: "1:549652082720:web:09bc0f371a498ee5184c45",
-  measurementId: "G-TGFHW9XKF2"
+  apiKey: "AIzaSyDDvRtYzrmOix6TF3ap2u5n8Bz1u8Mptdw",
+  authDomain: "final-adani.firebaseapp.com",
+  projectId: "final-adani",
+  storageBucket: "final-adani.firebasestorage.app",
+  messagingSenderId: "174751903869",
+  appId: "1:174751903869:web:763b0e23f421b09b9ec114",
+  measurementId: "G-8BPS95G0V7"
 };
 
 // Initialize Firebase
